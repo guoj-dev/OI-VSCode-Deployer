03fdbed02539d89dc2d3d9fb4f841f87d67949b7
Quank <Quank123Wip@outlook.com>
2019-05-13 12:34:55 +0800

    Create hello world.cpp

---
+++ hello world.cpp
@@ -0,0 +1,9 @@
+//you can test your config with debugging/compiling this file
+//use F5 to start a debug session
+#include<bits/stdc++.h>
+int main()
+{
+  std::cout<<"Hello World"<<std::endl;//C++ Standard
+  std::printf("Hello World!");//C Standard
+  return 0;
+}
